45d3995871779514b31904b1e757bf4c843f6f8d
alfianrht <alvianrht@gmail.com>
2023-02-07 02:16:40 +0700

    update template dashboard

---
+++ application/controllers/admin/Beranda.php
@@ -0,0 +1,12 @@
+<?php 
+class Beranda extends CI_Controller{
+    function __construct(){
+        parent::__construct();
+
+    }
+
+    function index(){
+
+		$this->load->view('admin/beranda');
+    }
+}   
