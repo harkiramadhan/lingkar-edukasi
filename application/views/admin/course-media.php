d9d9a7f87b3e022019857a1db74a535304ed16b5
alfianrht <alvianrht@gmail.com>
2023-03-05 21:50:28 +0700

    Add Course Media Add

---
+++ application/views/admin/course-media.php
@@ -0,0 +1,272 @@
+<div class="content-body" style="min-height: 1129px;">
+	<!-- row -->
+	<div class="container-fluid">
+		<div class="page-titles m-0 px-0 pt-0">
+			<ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="http://localhost/lingkar-edukasi/admin"><i class="fa fa-home" aria-hidden="true"></i></a></li>
+				<li class="breadcrumb-item"><a href="http://localhost/lingkar-edukasi/admin/course">Course</a></li>
+				<li class="breadcrumb-item active"><a href="javascript:void(0)">Perpajakan V2</a></li>
+			</ol>
+		</div>
+		<div class="row">
+			<div class="col-xl-8 col-xxl-6">
+                <div class="row">
+                    <div class="col-xl-12 col-md-6">
+                        <div class="card">
+                            <div class="card-header border-0 pb-0">
+                                <h4 class="fs-20 text-black"><i class="fa fa-file text-secondary mr-3" aria-hidden="true"></i>Daftar Media</h4>
+                                <div class="d-flex mb-0">
+                                    <button class="btn btn-primary text-nowrap" data-toggle="modal" data-target="#modal-tambah-media">+ Media</button>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                
+                                <div id="accordion-six" class="accordion accordion-left-indicator">
+                                    
+                                    <div class="accordion__item">
+                                        <div class="d-flex align-items-start">
+
+                                            <div class="accordion__header d-flex align-items-center mr-2" data-toggle="collapse" data-target="#item-satu">
+                                                <span class="accordion__header--icon"></span>
+                                                <i class="fa fa-video-camera pl-4 mr-3" aria-hidden="true"></i>
+                                                <span class="accordion__header--text pl-0">
+                                                    Materi 1 - Membahas Pajak Secara Khusus Membahas Pajak Secara Khusus Membahas Pajak Secara Khusus    
+                                                </span>
+                                                <span class="accordion__header--indicator style_two" ></span>
+                                            </div>
+                                            <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1"  data-toggle="modal" data-target="#modal-tambah-video"><i class="fa fa-plus"></i></button>
+                                            <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1"><i class="fa fa-pencil"></i></button>
+                                            <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                        </div>
+                                        <div id="item-satu" class="collapse accordion__body show" data-parent="#accordion-six">
+                                            <div class="accordion__body--text">
+                                                
+                                                <div class="accordion-text-item d-flex align-items-center mb-2">
+                                                    <i class="fa fa-video-camera pl-0 mr-3 ml-2" aria-hidden="true"></i>
+                                                    <p class="mb-0">                                                    
+                                                        Sub 1 - Membahas Pajak Secara Khusus 
+                                                    </p>
+
+                                                    <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1 ml-auto"><i class="fa fa-pencil"></i></button>
+                                                    <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                                </div>
+
+                                                <div class="accordion-text-item d-flex align-items-center mb-2">
+                                                    <i class="fa fa-video-camera pl-0 mr-3 ml-2" aria-hidden="true"></i>
+                                                    <p class="mb-0">                                                    
+                                                        Sub 2 - Membahas Pajak Secara Khusus 
+                                                    </p>
+
+                                                    <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1 ml-auto"><i class="fa fa-pencil"></i></button>
+                                                    <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion__item">
+                                        <div class="d-flex align-items-start">
+
+                                            <div class="accordion__header collapsed d-flex align-items-center mr-2" data-toggle="collapse" data-target="#item-dua">
+                                                <span class="accordion__header--icon"></span>
+                                                <i class="fa fa-video-camera pl-4 mr-3" aria-hidden="true"></i>
+                                                <span class="accordion__header--text pl-0">
+                                                    Materi 2 - Membahas Pajak Secara Khusus Membahas Pajak Secara Khusus Membahas Pajak Secara Khusus    
+                                                </span>
+                                                <span class="accordion__header--indicator style_two" ></span>
+                                            </div>
+                                            <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1"  data-toggle="modal" data-target="#modal-tambah-video"><i class="fa fa-plus"></i></button>
+                                            <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1"><i class="fa fa-pencil"></i></button>
+                                            <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                        </div>
+                                        <div id="item-dua" class="collapse accordion__body" data-parent="#accordion-six">
+                                            <div class="accordion__body--text">
+                                                
+                                                <div class="accordion-text-item d-flex align-items-center mb-2">
+                                                    <i class="fa fa-video-camera pl-0 mr-3 ml-2" aria-hidden="true"></i>
+                                                    <p class="mb-0">                                                    
+                                                        Sub 1 - Membahas Pajak Secara Khusus 
+                                                    </p>
+
+                                                    <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1 ml-auto"><i class="fa fa-pencil"></i></button>
+                                                    <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                                </div>
+
+                                                <div class="accordion-text-item d-flex align-items-center mb-2">
+                                                    <i class="fa fa-video-camera pl-0 mr-3 ml-2" aria-hidden="true"></i>
+                                                    <p class="mb-0">                                                    
+                                                        Sub 2 - Membahas Pajak Secara Khusus 
+                                                    </p>
+
+                                                    <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1 ml-auto"><i class="fa fa-pencil"></i></button>
+                                                    <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="accordion__item">
+                                        <div class="d-flex align-items-start">
+
+                                            <div class="accordion__header collapsed d-flex align-items-center mr-2" data-toggle="collapse" data-target="#item-tiga">
+                                                <span class="accordion__header--icon"></span>
+                                                <i class="fa fa-video-camera pl-4 mr-3" aria-hidden="true"></i>
+                                                <span class="accordion__header--text pl-0">
+                                                    Materi 3 - Membahas Pajak Secara Khusus Membahas Pajak Secara Khusus Membahas Pajak Secara Khusus    
+                                                </span>
+                                                <span class="accordion__header--indicator style_two" ></span>
+                                            </div>
+                                            <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1"  data-toggle="modal" data-target="#modal-tambah-video"><i class="fa fa-plus"></i></button>
+                                            <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1"><i class="fa fa-pencil"></i></button>
+                                            <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                        </div>
+                                        <div id="item-tiga" class="collapse accordion__body" data-parent="#accordion-six">
+                                            <div class="accordion__body--text">
+                                                
+                                                <div class="accordion-text-item d-flex align-items-center mb-2">
+                                                    <i class="fa fa-video-camera pl-0 mr-3 ml-2" aria-hidden="true"></i>
+                                                    <p class="mb-0">                                                    
+                                                        Sub 1 - Membahas Pajak Secara Khusus 
+                                                    </p>
+
+                                                    <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1 ml-auto"><i class="fa fa-pencil"></i></button>
+                                                    <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                                </div>
+
+                                                <div class="accordion-text-item d-flex align-items-center mb-2">
+                                                    <i class="fa fa-video-camera pl-0 mr-3 ml-2" aria-hidden="true"></i>
+                                                    <p class="mb-0">                                                    
+                                                        Sub 2 - Membahas Pajak Secara Khusus 
+                                                    </p>
+
+                                                    <button type="button" data-id="7" class="btn btn-dark btn-sm dark ml-0 px-2 py-1 mr-1 ml-auto"><i class="fa fa-pencil"></i></button>
+                                                    <button type="button" data-id="7" class="btn btn-danger btn-sm light ml-0 px-2 py-1 btn-remove"><i class="fa fa-trash"></i></button>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-xl-4 col-xxl-4">
+				<div class="row">
+					<div class="col-xl-12">
+						<div class="card event-detail-bx overflow-hidden">
+							<div class="card-media">
+                                <img src="http://localhost/lingkar-edukasi/uploads/courses/9d3c9b1cf80efe0ed18648caf9d44655.jpg" alt="" class="w-100" style="object-fit: cover;">
+                            </div>
+							<div class="card-body">
+								<div class="d-flex flex-wrap align-items-center mb-4">
+									<h2 class="text-black col-xl-12 p-0 col-xxl-12 mr-auto title mb-0">Kelas Cara Menyusun Pajak Tahunan di Akhir</h2>
+								</div>
+								<div class="row">
+									<div class="col-lg-12 col-md-12 col-xxl-6 mb-3">
+										<div class="media bg-light p-3 rounded align-items-center">	
+											
+											<div class="media-body">
+												<span class="fs-12 d-block mb-1">Harga Course</span>
+												<span class="fs-16 text-black">Rp. 1.520.000</span>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-12 col-md-6 col-xxl-6 mb-3">
+										<div class="media bg-light p-3 rounded align-items-center">	
+											
+											<div class="media-body">
+												<span class="fs-12 d-block mb-1">Tutor</span>
+												<span class="fs-16 text-black">Alfian RHT</span>
+											</div>
+										</div>
+									</div>
+                                    <div class="col-lg-6 col-md-6 col-xxl-6 mb-3">
+										<div class="media bg-light p-3 rounded align-items-center">	
+											
+											<div class="media-body">
+												<span class="fs-12 d-block mb-1">Jumlah Video</span>
+												<span class="fs-16 text-black">17</span>
+											</div>
+										</div>
+									</div>
+                                    <div class="col-lg-6 col-md-6 col-xxl-6 mb-3">
+										<div class="media bg-light p-3 rounded align-items-center">	
+											
+											<div class="media-body">
+												<span class="fs-12 d-block mb-1">Total Durasi</span>
+												<span class="fs-16 text-black">1 Jam</span>
+											</div>
+										</div>
+									</div>
+                                    
+								</div>
+								
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+
+<div class="modal fade" id="modal-tambah-media">
+	<div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Tambah Media</h5>
+                <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <form action="#" method="POST">
+                    <div class="form-group">
+                        <label class="text-black font-w500">Judul</label>
+                        <input name="label" type="text" class="form-control" value="" placeholder="Tulis judul" required>
+                    </div>
+
+                    <div class="form-group mb-0">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+	</div>
+</div>
+
+<div class="modal fade" id="modal-tambah-video">
+	<div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Tambah Video</h5>
+                <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
+            </div>
+            <div class="modal-body">
+                <form action="#" method="POST">
+                    <div class="form-group">
+                        <label class="text-black font-w500">Judul Video</label>
+                        <input name="label" type="text" class="form-control" value="" placeholder="Tulis judul" required>
+                    </div>
+                    
+                    <div class="form-group">
+                        <label class="text-black font-w500">Durasi Video</label>
+                        <input name="time" type="time" class="form-control" value="" required>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="text-black font-w500">Status</label>
+                        <select class="form-control default-select " name="status" required>
+                            <option value="" disabled>Pilih</option>
+                            <option value="1">Aktif</option>
+                            <option value="0">Tidak Aktif</option>
+                        </select>
+                    </div>
+                    <div class="form-group mb-0">
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+	</div>
+</div>
