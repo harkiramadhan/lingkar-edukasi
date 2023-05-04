1c9f4ebfbd3239cdf3b3094775fd05ac919e4bd8
harkiramadhan <harkiramadhan@gmail.com>
2023-05-04 21:40:50 +0700

    updates

--- application/controllers/Sertifikat.php
+++ application/controllers/Sertifikat.php
@@ -1,9 +1,23 @@
 <?php 
 class Sertifikat extends CI_Controller{
 
+  function __construct(){
+    parent::__construct();
+
+    $this->load->model([
+      'M_Settings',
+      'M_Labels'
+    ]);
+  }
+
   function index(){
-    $this->load->view('layout/user/header');
-    $this->load->view('user/cek-sertifikat');
-    $this->load->view('layout/user/footer');
+    $var = [
+      'labels' => $this->M_Labels->getActive(),
+      'setting' => $this->M_Settings->get()
+    ];
+
+    $this->load->view('layout/user/header', $var);
+    $this->load->view('user/cek-sertifikat', $var);
+    $this->load->view('layout/user/footer', $var);
   }
 }   
