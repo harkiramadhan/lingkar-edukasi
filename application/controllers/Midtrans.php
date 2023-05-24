c13d6ec03cf7f176665cf4710d735b76b238be7f
harkiramadhan <harkiramadhan@gmail.com>
2023-05-24 10:47:53 +0700

    updates

--- application/controllers/Midtrans.php
+++ application/controllers/Midtrans.php
@@ -180,7 +180,7 @@ class Midtrans extends CI_Controller{
         $mail = new PHPMailer(true);
 
         $mail->isSMTP();
-        $mail->SMTPDebug    = 2;
+        // $mail->SMTPDebug    = 2;
         $mail->Host         = 'mail.lingkaredukasi.com';
         $mail->SMTPAuth     = true;
         $mail->Username     = 'norep@lingkaredukasi.com';
