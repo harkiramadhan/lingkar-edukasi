0a7a8bb5cab11ec0220ee3ea106ab9f18e8b5560
Alfian Rahmatullah <alvianrht@gmail.com>
2023-05-30 00:08:35 +0700

    Update kelas done

--- application/views/user/kelassaya-selesai.php
+++ application/views/user/kelassaya-selesai.php
@@ -4,7 +4,7 @@
     <div class="padding-vertical padding-80">
       <div class="container">
         <div class="flex-vertical">
-          <div class="signin-image_middle"><img src="<?= base_url('assets/user/images/home-hero-mainimage.webp')?>" loading="lazy" sizes="(max-width: 479px) 93vw, (max-width: 767px) 94vw, 550px" srcset="images/home-hero-mainimage-p-500.png 500w, <?= base_url('assets/user/images/home-hero-mainimage.webp')?> 883w" alt="" class="signin-image"></div>
+          <div class="signin-image_middle"><img src="<?= base_url('assets/user/images/home-hero-mainimage.webp')?>" loading="lazy" sizes="(max-width: 479px) 93vw, (max-width: 767px) 94vw, 550px" srcset="<?= base_url('assets/user/images/home-hero-mainimage.webp')?> 500w, <?= base_url('assets/user/images/home-hero-mainimage.webp')?> 883w" alt="" class="signin-image"></div>
           <div class="verify-content">
             <h1 class="semibolld text-align-center">Wow, Kelas Telah Selesai</h1>
             <p class="blind-text width-80">Download sertifikatmu sekarang juga</p>
