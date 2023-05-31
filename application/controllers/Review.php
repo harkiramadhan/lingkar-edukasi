93f4d6339570cb69217041c305be0fb40f385abd
Ahmad Harki Ramadhan <harkiramadhan@Harkis-Mac-mini.local>
2023-05-31 09:41:46 +0700

    updates

--- application/controllers/Review.php
+++ application/controllers/Review.php
@@ -4,9 +4,9 @@ class Review extends CI_Controller{
       parent::__construct();
       $this->load->model([
         'M_Users',
-        // 'M_Courses',
-        // 'M_Settings',
-        // 'M_Labels',
+        'M_Courses',
+        'M_Settings',
+        'M_Labels',
         // 'M_Benefit_Landing',
         // 'M_Testimoni_Landing',
         // 'M_Banners',
@@ -14,16 +14,19 @@ class Review extends CI_Controller{
       ]);
     }
 
-  function index(){
+  function index($flag){
+    $userid = $this->session->userdata('user_id');
+    $course = $this->M_Courses->getByFlag($flag);
 
-
-    if($this->session->userdata('is_user')){
-        $userid = $this->session->userdata('user_id');
-        $var['user'] = $this->M_Users->getById($userid);
-      }
+    $var = [
+      'labels' => $this->M_Labels->getActive(),
+      'setting' => $this->M_Settings->get(),
+      'user' => $this->M_Users->getById($userid),
+      'course' => $course
+    ];
 
     $this->load->view('layout/user/header', $var);
     $this->load->view('user/kelassaya-selesai', $var);
     $this->load->view('layout/user/footer', $var);
   }
-}   
+}
