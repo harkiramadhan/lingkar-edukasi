f37d87d40fda2b061d5f139e76f7a1b48e1a5dd0
harkiramadhan <harkiramadhan@gmail.com>
2023-06-05 22:54:12 +0700

    updates

---
+++ application/models/M_Reviews.php
@@ -0,0 +1,31 @@
+<?php
+class M_Reviews extends CI_Model{
+    function getAll(){
+        return $this->db->select('u.name, c.judul, r.*')
+                        ->from('review r')
+                        ->join('user u', 'r.userid = u.id')
+                        ->join('courses c', 'r.courseid = c.id')
+                        ->order_by('id', "DESC")->get();
+    }
+
+    function getById($id){
+        return $this->db->select('u.name, c.judul, r.*')
+                        ->from('review r')
+                        ->join('user u', 'r.userid = u.id')
+                        ->join('courses c', 'r.courseid = c.id')
+                        ->where('r.id', $id)->get()->row();
+    }
+
+    function getByCourses($courseid, $limit = false){
+        if($limit){
+            $this->db->limit($limit);
+        }
+
+        return $this->db->select('u.name, c.judul, r.*')
+                        ->from('review r')
+                        ->join('user u', 'r.userid = u.id')
+                        ->join('courses c', 'r.courseid = c.id')
+                        ->where('r.courseid', $courseid)
+                        ->order_by('id', "DESC")->get();
+    }
+}
---
+++ assets/admin/js/custom/review.js
@@ -0,0 +1,59 @@
+$('.btn-edit').click(function(){
+    var id = $(this).attr('data-id')
+    $.ajax({
+        url: baseUrl + 'admin/reviews/edit/' + id,
+        type: 'get',
+        beforeSend: function(){
+            $('.edit-content').empty()
+            $('#modal-edit').modal('show')
+        },
+        success: function(res){
+            $('.edit-content').html(res)
+        }
+    })
+})
+
+$('.btn-remove').click(function(){
+    var id = $(this).attr('data-id')
+
+    Swal.fire({
+        icon: 'error',
+        title: "Are you sure to delete ?",
+        text: "You will not be able to recover this imaginary file !!",
+        type: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#DD6B55",
+        confirmButtonText: "Yes, delete it !!",
+        cancelButtonText: "No, cancel it !!",
+        closeOnConfirm: false,
+        closeOnCancel: false
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: baseUrl + 'admin/reviews/remove/' + id,
+                type: 'post',
+                error: function(){
+                    Swal.fire({
+                        icon: 'error',
+                        title: 'Oops...',
+                        text: 'Something went wrong!'
+                    })
+                },
+                success: function(res){
+                    if(res == 1){
+                        $('#data-' + id).remove()
+                        Swal.fire("Deleted !!", "Hey, your imaginary file has been deleted !!", "success")
+                    }else{
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Oops...',
+                            text: 'Something went wrong!'
+                        })
+                    }
+                }
+            })
+        } else if (result.isDenied) {
+            Swal.fire('Changes are not saved', '', 'info')
+        }
+    })
+})
